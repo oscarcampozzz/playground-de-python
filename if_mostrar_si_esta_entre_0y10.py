89bf8bd4c2aedec6341152dc59f0a0593362de52
oscarcampozzz <oscarillo243@gmail.com>
2021-07-16 10:36:24 -0600

    solo se modifico el de repaso que siempre se modifica xD

--- if_mostrar_si_esta_entre_0y10.py
+++ if_mostrar_si_esta_entre_0y10.py
@@ -2,7 +2,7 @@
 
 
 
-a=0
+a=5
 
 if(a>=1 and a<=10):
     print("Esta entre 1 y 10")
--- para_repaso.py
+++ para_repaso.py
@@ -1,3 +1,4 @@
-entradas = [3,8,9,6,7]
-for i in entradas:
-    print(i)
+cadena = "prueba para convertir a conjunto "
+
+conjunto = set(cadena)
+print(conjunto)
